b5e1cab27a8eccbd96991106af6a14cd38785b91
Xavier Collantes <xcollantes@google.com>
2019-08-27 22:10:34 -0700

    Upddate

--- LinkedList.py
+++ LinkedList.py
@@ -9,3 +9,19 @@ class Node(object):
     self.data = data
     self.next = None
 
+  def GetData():
+    return self.data
+
+
+  def Size(acc):
+    """Determines the size of the linked list.
+
+    acc: Number of nodes in list, used for recursive call.
+
+    Returns: Int of the current head to the end of list.
+    """
+    if self.next == None:
+      return 1
+    else:
+      return 0 #(TODO): Should this be implemented in Singly linked?
+
