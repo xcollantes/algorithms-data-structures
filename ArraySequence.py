c7e8a36e333f3379b2bd32b3791b436846f42b96
xcollantes <xcollantes@zagmail.gonzaga.edu>
2019-09-01 15:25:04 -0700

    Refactored the ArraySequence to confirm to Google standard

--- ArraySequence.py
+++ ArraySequence.py
@@ -1,16 +1,30 @@
 """Array Sequence Practice
 """
-
-
 __author__ = 'Xavier Collantes'
 
 
 def main():
-  print('Anagram Check, Solution 1: %s' % anagram_check('clint eastwood', 'old west action'))
-  print('Anagram Check, Solution 2: %s' % anagram_check2('clint eastwood', 'old west action'))
-  print('Array Pairs, Solution 1: %s' % array_pair([1, 3, 2, 2], 4))
+  print('Anagram Check, Solution 1: %s' % AnagramCheck('clint eastwood', 'old west action'))
+  print('Anagram Check, Solution 2: %s' % AnagramCheck2('clint eastwood', 'old west action'))
+  print('Array Pairs, Solution 1: %s' % ArrayPair([1, 3, 2, 2], 4))
+  print('Finder: %s' % Finder([1,2,3,4,5,6,7,8,9],[]))
 
-def array_pair(arr, sum):
+
+def Finder(list, shuffled_list):
+  """Find the missing element in shuffled_list.
+
+  Given two lists, the second a shuffled version of first, return
+  the element missing in shuffled_list.
+
+  list: Ordered list of non-negative numbers.
+  shuffled_list: Randomized version of list with one element missing.
+
+  Returns: The missing element not in shuffled_list but present
+  in list.
+  """
+
+
+def ArrayPair(arr, sum):
   """Find the pairs in given array that sum up to the given sum.
   """
   eval = []
@@ -22,7 +36,7 @@ def array_pair(arr, sum):
   return eval
 
 
-def anagram_check(x, y):
+def AnagramCheck(x, y):
   ''' Anagram Check:  Check to see if both inputs are anagrams of each other. 
       Solution 1: Sort strings then compare since both should have the same 
       number of letters. 
@@ -33,7 +47,7 @@ def anagram_check(x, y):
   return sorted(x) == sorted(y)
 
 
-def anagram_check2(x, y):
+def AnagramCheck2(x, y):
   '''Solution 2: Verify two inputs are anagrams.  
   '''
   x = x.replace(' ', '').lower()
