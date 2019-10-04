fbb7fdd07b7f22306d9f5614b4cf6cc9a4da04a7
xcollantes <xcollantes@zagmail.gonzaga.edu>
2019-10-03 19:12:14 -0700

    Add SelectionSort sovled

--- QuickSort.py
+++ QuickSort.py
@@ -6,36 +6,36 @@ from typing import List
 def main():
   list = [5,3,8,0,2,4,2,9,0]
   print(f'LIST: {list}')
-  quickSort(list) 
+  lomutoQuickSort(list)
   print(f'SORT: {list}')
 
 
-def quickSort(arr: List[int]) -> List[int]:
-  quickSortHelper(arr, 0, len(arr) - 1) 
+def lomutoQuickSort(arr: List[int]) -> List[int]:
+  lomutoQuickSortHelper(arr, 0, len(arr) - 1)
 
 
-def quickSortHelper(arr: List[int], first, last) -> List[int]:
+def lomutoQuickSortHelper(arr: List[int], first, last) -> List[int]:
   if first < last:
 
-    split = partition(arr, first, last)
+    split = lomutoPartition(arr, first, last)
 
-    quickSortHelper(arr, first, split - 1)
-    quickSortHelper(arr, split + 1, last)
-    
+    lomutoQuickSortHelper(arr, first, split - 1)
+    lomutoQuickSortHelper(arr, split + 1, last)
 
-def partition(arr: List[int], first, last):
+
+def lomutoPartition(arr: List[int], first, last):
   pivot = arr[first]
-  
+
   left = first + 1
   right = last
 
   done = False
 
   while not done:
-    
+
     while left <= right and arr[left] <= pivot:
       left += 1
-    
+
     while arr[left] >= pivot and right >= left:
       right -= 1
 
