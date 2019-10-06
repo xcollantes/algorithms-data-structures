9dfff80c70b67f339aea8cbe9acd52e869d65f77
xcollantes <xcollantes@zagmail.gonzaga.edu>
2019-10-05 21:03:46 -0700

    StringPermutations

---
+++ interview_problems/StringPermutations.py
@@ -0,0 +1,29 @@
+"""Return all permutations of given string."""
+
+import unittest
+from typing import List
+
+class Permute:
+  def __init__(self):
+    pass
+
+  def perm(text: str) -> List[str]:
+    
+
+
+
+
+class PermuteTest(unittest.TestCase):
+
+  @unittest.skip('Setup not')
+  def setUp(self):
+    self.s = 'abcde'
+    self.p = Permute()
+
+  def allPerms(self):
+    result = ['']
+    assertEqual(Permute.perm(), result)
+
+
+if __name__=='__main__':
+  unittest.main()
