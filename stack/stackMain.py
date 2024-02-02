290aac9b45c6757450447725023eb5b958744527
Xavier <collantes.xavier@gmail.com>
2024-02-02 01:52:26 -0800

    Change format.
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- stack/stackMain.py
+++ stack/stackMain.py
@@ -1,24 +1,21 @@
-# Implementation of Stacks
-
-__author__ = 'Xavier Collantes'
+"""Implementation of Stacks."""
 
 from stack.Stack import Stack
 
+
 def main():
-  s = Stack()
-  print('Empty?: %s' % s.IsEmpty())
-  s.Push('Dazed and Confused')
-  s.Push('I Can\'t Quit You Baby')
-  s.Push('Wearing and Tearing')
-  print(s.Peek())
-  print(s.Peek())
-  print('Size: %s' % s.Size())
-  s.Pop()
+    s = Stack()
+    print("Empty?: %s" % s.IsEmpty())
+    s.Push("Dazed and Confused")
+    s.Push("I Can't Quit You Baby")
+    s.Push("Wearing and Tearing")
+    print(s.Peek())
+    print(s.Peek())
+    print("Size: %s" % s.Size())
+    s.Pop()
 
-  print('Size: %s' % s.Size())
-
-
+    print("Size: %s" % s.Size())
 
 
-if __name__=='__main__':
-  main()
+if __name__ == "__main__":
+    main()
