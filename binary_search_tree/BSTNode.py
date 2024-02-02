708a99c9aa7996d89d20a7eae86f6d83e1c2d127
Xavier <collantes.xavier@gmail.com>
2024-02-02 01:50:48 -0800

    Formatting
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- binary_search_tree/BSTNode.py
+++ binary_search_tree/BSTNode.py
@@ -1,31 +1,30 @@
 """Binary Search Trees Practice.
 """
-__author__ = 'Xavier Collantes'
+
+__author__ = "Xavier Collantes"
 
 
 class BSTNode:
-  def __init__(self, key, data=None):
-    self.key = key
-    self.data = data
-    self.left = None
-    self.right = None
+    def __init__(self, key, data=None):
+        self.key = key
+        self.data = data
+        self.left = None
+        self.right = None
 
-  def GetRight(self):
-    return self.right
+    def GetRight(self):
+        return self.right
 
-  def GetLeft(self):
-    return self.left
+    def GetLeft(self):
+        return self.left
 
-  def SetRight(self, data):
-    self.data = data
+    def SetRight(self, data):
+        self.data = data
 
-  def SetLeft(self, data):
-    self.data = data
+    def SetLeft(self, data):
+        self.data = data
 
-  def GetKey(self):
-    return self.key
+    def GetKey(self):
+        return self.key
 
-  def GetData(self):
-    return self.data
-
-
+    def GetData(self):
+        return self.data
