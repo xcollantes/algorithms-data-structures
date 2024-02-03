2ff527e08851e1fcc6cfca40840e53cbae535d85
Xavier <collantes.xavier@gmail.com>
2024-02-02 18:39:25 -0800

    Add insertion sort
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -10,14 +10,18 @@ def insertion(input: list) -> list:
     if len(input) < 1:
         return []
 
+    # Iterates over the array O(n)
     for stepIdx in range(1, len(input)):
+
         temp_value: any = input[stepIdx]
         prevIdx: any = stepIdx - 1
 
+        print(f"input: {input}; prevIdx: {prevIdx}; temp: {temp_value};")
+
         while temp_value < input[prevIdx] and prevIdx >= 0:
-            input[prevIdx] = temp_value
+            input[prevIdx + 1] = input[prevIdx]
             prevIdx -= 1
 
-        input[stepIdx] = temp_value
+        input[prevIdx + 1] = temp_value
 
     return input
