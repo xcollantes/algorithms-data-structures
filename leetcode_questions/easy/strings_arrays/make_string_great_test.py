d3dff0eb6e08d0f5d8b782bdbeefa648ffbb160c
Xavier <collantes.xavier@gmail.com>
2024-02-23 16:45:43 -0800

    String
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

---
+++ leetcode_questions/easy/strings_arrays/make_string_great_test.py
@@ -0,0 +1,15 @@
+"""Unit test for make_string_great.py."""
+
+import unittest
+import logging
+
+from leetcode_questions.easy.strings_arrays.make_string_great import make_great
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+class TestMakeStringGreat(unittest.TestCase):
+    def test_make_string_great(self):
+        self.assertEqual(make_great("leEeetcode"), "leetcode")
+        self.assertEqual(make_great("abBAcC"), "")
+        self.assertEqual(make_great("s"), "s")
