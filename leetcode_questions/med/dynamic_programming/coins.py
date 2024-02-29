76fa7e9c0243467030779df3b7242a908c076b76
Xavier <collantes.xavier@gmail.com>
2024-02-29 13:58:44 -0800

    Coin bag comments
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- leetcode_questions/med/dynamic_programming/coins.py
+++ leetcode_questions/med/dynamic_programming/coins.py
@@ -13,11 +13,12 @@ def coin(coins: list[int], amount: int) -> int:
     """Return fewest of each coin to make amount."""
     # Create array of min coins to make each index
     min_coins: list[int] = [amount + 1] * (amount + 1)
-
     logging.info("min_coins: %s", min_coins)
 
     min_coins[0] = 0
 
+    # Each value is the number of coins needed for that index
+    # So index 1 needs 1 coin assuming 1 coin is an option in the coins options
     for idx in range(len(min_coins)):
         logging.info("IDX: %s", idx)
         for coin in coins:
@@ -27,11 +28,16 @@ def coin(coins: list[int], amount: int) -> int:
                     " new min: %s", min(min_coins[idx], min_coins[idx - coin] + 1)
                 )
 
+                # Refer back to the idx - coin then add 1 coin;
+                # Iterate the coins until you get min number of coins
                 min_coins[idx] = min(min_coins[idx], min_coins[idx - coin] + 1)
 
                 logging.info(" min_coins: %s", min_coins)
 
+    # Check if the amount-th index is still the default set in the beginning
     if min_coins[amount] == amount + 1:
         return -1
 
+    # Return the amount-th index which is the number of coins needed for that
+    # amount index
     return min_coins[amount]
