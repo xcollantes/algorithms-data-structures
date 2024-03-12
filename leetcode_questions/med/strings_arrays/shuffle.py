e31ad6acf499bd4f399cfad276f92558d81d0f08
Xavier <collantes.xavier@gmail.com>
2024-03-11 19:32:00 -0700

    Add shuffle
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- leetcode_questions/med/strings_arrays/shuffle.py
+++ leetcode_questions/med/strings_arrays/shuffle.py
@@ -8,14 +8,37 @@ int[] reset() Resets the array to its original configuration and returns it.
 int[] shuffle() Returns a random shuffling of the array.
 """
 
+import copy
+import logging
+import random
+
 
 class Shuffle:
     def __init__(self, nums: list[int]) -> None:
         self.nums = nums
-        self.const_nums = nums
+        # Technically a shallow copy works since nums is single dimensional
+        self.const_nums = copy.deepcopy(nums)
 
     def reset(self) -> list[int]:
         """Resets the array to its original configuration and returns it."""
+        return self.nums
+
+    # This technically works but could possibly repeat.
+    #
+    # def shuffle(self) -> list[int]:
+    #     """Returns a random shuffling of the array."""
+    #     return random.sample(self.nums, len(self.nums))
 
     def shuffle(self) -> list[int]:
-        """Returns a random shuffling of the array."""
+        """Returns a random shuffling of the array.
+
+        Uses the Fisher-Yates algorithm.
+        """
+        answer: list[int] = self.nums[:]  # Shallow copy
+        for idx in range(len(answer)):
+            logging.info(answer)
+            rand_idx = random.randrange(idx, len(answer))
+            logging.info(rand_idx)
+            answer[rand_idx], answer[idx] = answer[idx], answer[rand_idx]
+
+        return answer
--- leetcode_questions/med/strings_arrays/shuffle_test.py
+++ leetcode_questions/med/strings_arrays/shuffle_test.py
@@ -3,9 +3,23 @@
 import unittest
 import logging
 
+from leetcode_questions.med.strings_arrays.shuffle import Shuffle
+
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
 class TestShuffle(unittest.TestCase):
     def test_shuffle(self):
-        self.assertEqual("", "")
+        a = [1, 2, 3]
+        s = Shuffle(a)
+
+        prev = []
+        x = s.shuffle()
+        prev.append(x)
+        self.assertNotEqual(x, a)
+
+        self.assertEqual(s.reset(), a)
+
+        i = s.shuffle()
+        prev.append(i)
+        self.assertNotIn(a, prev)
