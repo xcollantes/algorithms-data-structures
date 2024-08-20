8479e292c7c2093ce8bbed0b9b96b64850fc3544
Xavier <collantes.xavier@gmail.com>
2024-08-20 16:35:18 -0700

    Add obstacles
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- leetcode_questions/hard/bfs/shortest_path_obstacles.py
+++ leetcode_questions/hard/bfs/shortest_path_obstacles.py
@@ -7,11 +7,55 @@ the lower right corner (m - 1, n - 1) given that you can eliminate at most k
 obstacles. If it is not possible to find such walk return -1.
 """
 
+from collections import deque
+
 
 def quickest_path(grid: list[list[int]], k: int) -> int:
-    for x in range(len(grid)):
-        for y in range(grid[0]):
-            
+    ROWS: int = len(grid)
+    COLS: int = len(grid[0])
+
+    queue = deque([])
+    # x, y, steps, k
+    queue.append((0, 0, 0, k))
+
+    # Stores x, y, k as a combination.
+    # or instance, you could reach a cell (r, c):
+    #   After having eliminated 2 obstacles (rk = k - 2).
+    #   After having eliminated 1 obstacle (rk = k - 1).
+    # A set would be faster with read of O(n).
+    visited: list[tuple[int, int, int]] = list()
+
+    while queue:
+        # Popleft since BFS. Popright would work too for DFS.
+        x, y, steps, k_remain = queue.popleft()
+        print(f"R: {x} C: {y}")
+        print(f"visited: {visited}")
+        print(
+            f"queue: (x:{x}, y:{y}, step:{steps}, k_remaining:{k_remain})",
+            end="\n\n",
+        )
+
+        if x == ROWS - 1 and y == COLS - 1:
+            return steps
+
+        print("not done")
+        for x_offset, y_offset in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            if (
+                0 <= x + x_offset
+                and x + x_offset < ROWS
+                and 0 <= y + y_offset
+                and y + y_offset < COLS
+                and (x + x_offset, y + y_offset, k_remain) not in visited
+            ):
+                if grid[x + x_offset][y + y_offset] == 1 and k_remain > 0:
+                    visited.append((x + x_offset, y + y_offset, k_remain))
+                    queue.append((x + x_offset, y + y_offset, steps + 1, k_remain - 1))
+
+                elif grid[x + x_offset][y + y_offset] == 0:
+                    visited.append((x + x_offset, y + y_offset, k_remain))
+                    queue.append((x + x_offset, y + y_offset, steps + 1, k_remain))
+
+    return -1
 
 
 def p(grid) -> None:
--- leetcode_questions/hard/bfs/shortest_path_obstacles_test.py
+++ leetcode_questions/hard/bfs/shortest_path_obstacles_test.py
@@ -15,6 +15,8 @@ class TestShortestPathObstacles(unittest.TestCase):
         # path with one obstacle elimination at position (3,2) is 6. Such path
         # is (0,0) -> (0,1) -> (0,2) -> (1,2) -> (2,2) -> (3,2) -> (4,2).
         # https://assets.leetcode.com/uploads/2021/09/30/short1-grid.jpg
+        #
+        # https://docs.google.com/spreadsheets/d/11Ugiz7lpMj9YyUmsbkE9BWzQGxQnoYu0AWoZbOS6lYQ/edit?usp=sharing
         self.assertEqual(
             quickest_path(
                 grid=[[0, 0, 0], [1, 1, 0], [0, 0, 0], [0, 1, 1], [0, 0, 0]], k=1
