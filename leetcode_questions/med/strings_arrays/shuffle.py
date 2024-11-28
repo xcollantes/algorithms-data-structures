61a0c33e7aabcfb188080cc4ae79eb8f060e1491
Xavier <collantes.xavier@gmail.com>
2024-11-27 21:05:26 -0800

    Better answer
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- leetcode_questions/med/strings_arrays/shuffle.py
+++ leetcode_questions/med/strings_arrays/shuffle.py
@@ -1,11 +1,13 @@
 """
-Given an integer array nums, design an algorithm to randomly shuffle the array. All permutations of the array should be equally likely as a result of the shuffling.
+Given an integer array nums, design an algorithm to randomly shuffle the array.
+All permutations of the array should be equally likely as a result of the
+shuffling.
 
 Implement the Solution class:
 
-Solution(int[] nums) Initializes the object with the integer array nums.
-int[] reset() Resets the array to its original configuration and returns it.
-int[] shuffle() Returns a random shuffling of the array.
+Solution(int[] nums) Initializes the object with the integer array nums.  int[]
+reset() Resets the array to its original configuration and returns it.  int[]
+shuffle() Returns a random shuffling of the array.
 """
 
 import copy
@@ -21,27 +23,27 @@ class Shuffle:
         # original data structure.
         self.ORIGINAL = copy.deepcopy(nums)
 
-        # Track position since Fisher-Yates algorithm depends on moving index
-        # for each "shuffle" call.
-        self.position = 0
-
     def reset(self) -> list[int]:
         self.nums = self.ORIGINAL
         return self.nums
 
     def shuffle(self) -> list[int]:
-        # Fisher-Yates algorithm chooses a random index and a moving index.
-        # The random index and moving index switch values to create a random
-        # value each time but a new random set each call.
+        # Fisher-Yates algorithm chooses a random index and a moving index.  The
+        # random index and moving index switch values to create a random value
+        # each time but a new random set each call.
 
-        random_idx: int = random.randint(
-            self.position, len(self.nums) - 1
-        )  # - 1 since random includes right value.
+        for i in range(len(self.nums)):
+            # Track position since Fisher-Yates algorithm depends on moving
+            # index for each "shuffle" call.
 
-        # Switch the random index and moving index.
-        self.nums[random_idx], self.nums[self.position] = (
-            self.nums[self.position],
-            self.nums[random_idx],
-        )
+            random_idx: int = random.randint(
+                i, len(self.nums) - 1
+            )  # - 1 since random includes right value.
+
+            # Switch the random index and moving index.
+            self.nums[random_idx], self.nums[i] = (
+                self.nums[i],
+                self.nums[random_idx],
+            )
 
         return self.nums
