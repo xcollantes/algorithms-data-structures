f21a2c149e9b8b1a8dc33d691325e48ceccad624
Xavier <collantes.xavier@gmail.com>
2024-12-06 22:41:59 -0800

    Add simple tests
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- leetcode_questions/med/strings_arrays/rotate_array_test.py
+++ leetcode_questions/med/strings_arrays/rotate_array_test.py
@@ -13,10 +13,16 @@ from leetcode_questions.med.strings_arrays.rotate_array import rotate, rotate_gr
 
 class TestRotateArray(unittest.TestCase):
     def test_rotate_array(self):
+        self.assertEqual(
+            rotate(nums=[0, 1, 2, 3, 4, 5, 6, 7], k=3), [5, 6, 7, 0, 1, 2, 3, 4]
+        )
         self.assertEqual(rotate(nums=[1, 2, 3, 4, 5, 6, 7], k=3), [5, 6, 7, 1, 2, 3, 4])
         self.assertEqual(rotate(nums=[-1, -100, 3, 99], k=2), [3, 99, -1, -100])
 
     def test_rotate_group_array(self):
+        self.assertEqual(
+            rotate_group(nums=[0, 1, 2, 3, 4, 5, 6, 7], k=3), [5, 6, 7, 0, 1, 2, 3, 4]
+        )
         self.assertEqual(
             rotate_group(nums=[1, 2, 3, 4, 5, 6, 7], k=3), [5, 6, 7, 1, 2, 3, 4]
         )
