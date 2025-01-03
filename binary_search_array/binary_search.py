033d161a1188df78f082da073a56d7d2e46d55a1
Xavier <collantes.xavier@gmail.com>
2025-01-03 00:42:55 -0800

    Add recursion
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- binary_search_array/binary_search.py
+++ binary_search_array/binary_search.py
@@ -20,8 +20,10 @@ mid = 5 + 8 // 2 = 6
                   L   M       R
 """
 
+import unittest
 
-def search(target: int, A: list[int]) -> int:
+
+def search_iterative(target: int, A: list[int]) -> int:
     """Returns the index where element found None otherwise"""
 
     # Initiate left and right indices as edges of the array.
@@ -57,10 +59,75 @@ def search(target: int, A: list[int]) -> int:
     return None
 
 
-# Must be sorted first.
-A = [1, 3, 7, 10, 12, 15, 20, 22, 25]
+def recursive(s: list[int], left: int, right: int, target: int) -> int:
+    """Recursive approach retaining the array passed in."""
 
-print(f"result: {search(20, A)}")
-assert search(20, A) == 6
-assert search(10, A) == 3
-assert search(40, A) == None
+    print(f"left: {left} right: {right}")
+
+    if left <= right:
+
+        mid = (left + right) // 2
+
+        print(f"mid: {mid}")
+
+        if s[mid] == target:
+            return mid
+
+        elif target < s[mid]:
+            return recursive(s, left, mid - 1, target)
+
+        elif target > s[mid]:
+            return recursive(s, mid + 1, right, target)
+
+    else:
+        return None
+
+
+def recursive_partitions(s: list[int], target: int) -> int:
+    """Recursive approach passing in partitions.
+
+    Instead of the whole array, partitions of the array are passed in. There is
+    no longer a need for left and right pointers but there is no way to track
+    indices if that is the required result.
+    """
+
+    print(f"s: {s}")
+
+    # Base case where not found.
+    if not s:
+        return None
+
+    mid = len(s) // 2
+
+    print(f"mid: {mid}")
+
+    if s[mid] == target:
+        return s[mid]
+
+    elif target < s[mid]:
+        return recursive_partitions(s[:mid], target=target)
+
+    elif s[mid] < target:
+        return recursive_partitions(s[mid:], target=target)
+
+
+class Test(unittest.TestCase):
+
+    # Must be sorted first.
+    A = [1, 3, 7, 10, 12, 15, 20, 22, 25]
+
+    def test_iterative(self) -> None:
+        self.assertEqual(search_iterative(20, self.A), 6)
+        self.assertEqual(search_iterative(10, self.A), 3)
+        self.assertEqual(search_iterative(40, self.A), None)
+
+    def test_recursive(self) -> None:
+        self.assertEqual(recursive(self.A, 0, len(self.A) - 1, 20), 6)
+        self.assertEqual(recursive(self.A, 0, len(self.A) - 1, 10), 3)
+        self.assertEqual(recursive(self.A, 0, len(self.A) - 1, 40), None)
+
+    def test_partitions(self) -> None:
+        # Cannot return the index but the value instead.
+        self.assertEqual(recursive_partitions(self.A, 20), 20)
+        self.assertEqual(recursive_partitions(self.A, 10), 10)
+        self.assertEqual(recursive_partitions(self.A, 40), None)
