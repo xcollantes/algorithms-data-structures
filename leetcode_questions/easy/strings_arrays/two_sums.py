c9d3876c0e5c6cb984301f4bf906b2f6da9477cd
Xavier <collantes.xavier@gmail.com>
2025-04-21 23:27:02 -0700

    Refactor two sums tests to include a new faster_two_sums function. Consolidate test cases into a reusable method for better maintainability.
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- leetcode_questions/easy/strings_arrays/two_sums.py
+++ leetcode_questions/easy/strings_arrays/two_sums.py
@@ -1,4 +1,7 @@
-"""
+"""1. Two Sum
+
+https://leetcode.com/problems/two-sum/
+
 Given an array of integers nums and an integer target, return indices of the two
 numbers such that they add up to target.
 
@@ -10,9 +13,28 @@ You can return the answer in any order.
 
 
 def two_sums(array: list[int], target: int) -> list[int]:
+    """O(n^2)"""
     for a in range(len(array) - 1):
         b = a + 1
         while b < len(array):
             if array[a] + array[b] == target:
                 return [a, b]
             b += 1
+
+
+def faster_two_sums(array: list[int], target: int) -> list[int]:
+    """O(n)"""
+
+    # {complement: index}
+    comp_map = {}
+
+    for i in range(len(array)):
+        complement = target - array[i]
+
+        # Check if missing complement has been seen.
+        if complement in comp_map:
+            return [comp_map[complement], i]
+
+        comp_map[array[i]] = i
+
+    return []
--- leetcode_questions/easy/strings_arrays/two_sums_test.py
+++ leetcode_questions/easy/strings_arrays/two_sums_test.py
@@ -2,11 +2,18 @@
 
 import unittest
 
-from leetcode_questions.two_sums import two_sums
+from leetcode_questions.easy.strings_arrays.two_sums import faster_two_sums, two_sums
 
 
 class TestTwoSums(unittest.TestCase):
+
+    def cases(self, fn):
+        self.assertEqual(fn([1, 2], 3), [0, 1])
+        self.assertEqual(fn([2, 3, 5, 14, 543, 23], 25), [0, 5])
+        self.assertEqual(fn([56, 345, 0, 2, 4, 5], 2), [2, 3])
+
     def test_twosums(self):
-        self.assertEqual(two_sums([1, 2], 3), [0, 1])
-        self.assertEqual(two_sums([2, 3, 5, 2, 14, 543, 23], 25), [0, 6])
-        self.assertEqual(two_sums([56, 345, 0, 2, 4, 5], 2), [2, 3])
+        self.cases(two_sums)
+
+    def test_faster_two_sums(self):
+        self.cases(faster_two_sums)
