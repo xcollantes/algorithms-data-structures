7ea47dfc2cb0ffa31cbb7e2251c09e05c11cf970
Xavier <collantes.xavier@gmail.com>
2025-04-24 00:49:25 -0700

    Remove unit tests for missing_num and optimized_missing_num from a separate test file and integrate them directly into missing_num.py for better organization and accessibility.
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- leetcode_questions/easy/strings_arrays/missing_num.py
+++ leetcode_questions/easy/strings_arrays/missing_num.py
@@ -37,3 +37,18 @@ def optimized_missing_num(nums: list[int]) -> int:
             return idx
 
     return len(nums)
+
+
+# pytest missing_num.py
+
+
+def test_missing():
+    assert missing_num([3, 0, 1]) == 2
+    assert missing_num([0, 1]) == 2
+    assert missing_num([9, 6, 4, 2, 3, 5, 7, 0, 1]) == 8
+
+
+def test_optimized():
+    assert optimized_missing_num([3, 0, 1]) == 2
+    assert optimized_missing_num([0, 1]) == 2
+    assert optimized_missing_num([9, 6, 4, 2, 3, 5, 7, 0, 1]) == 8
