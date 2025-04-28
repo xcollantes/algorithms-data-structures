dd6b80cc9b1446597f66bbea0be1a8aea08b105a
Xavier <collantes.xavier@gmail.com>
2025-04-28 12:52:54 -0700

    Add working and comments to min stack
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- leetcode_questions/med/stacks_queues/min_stack.py
+++ leetcode_questions/med/stacks_queues/min_stack.py
@@ -1,94 +1,81 @@
 """155. Min Stack
 
+Medium
+
 Design a stack that supports push, pop, top, and retrieving the minimum element
 in constant time.
 
 Implement the MinStack class:
 
-MinStack() initializes the stack object.  void push(int val) pushes the element
-val onto the stack.  void pop() removes the element on the top of the stack.
-int top() gets the top element of the stack.  int getMin() retrieves the minimum
-element in the stack.  You must implement a solution with O(1) time complexity
-for each function.
-"""
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
 
-from collections import deque
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+Constraints:
+
+-231 <= val <= 231 - 1
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+At most 3 * 104 calls will be made to push, pop, top, and getMin.
+"""
 
 
 class MinStack:
     def __init__(self) -> None:
-        self.stack = deque()
-        self.minVal = deque()
+        self.mins = []
+        self.values = []
 
     def push(self, val: int) -> None:
-        print(f"PUSH: {val}")
+        self.values.append(val)
 
-        if len(self.minVal) <= 0:
-            self.minVal.append(val)
-            print(f"NEW MINVAL: {self.minVal}")
+        if self.mins:
+            self.mins.append(min(self.getMin(), val))
+        else:
+            self.mins.append(val)
 
-        elif self.minVal[-1] >= val:
-            self.minVal.append(val)
-            print(f"NEW MINVAL: {self.minVal}")
-
-        self.stack.append(val)
+        print(f"push: {val} m: {self.mins} v: {self.values}")
 
     def pop(self) -> int:
-        p = self.stack.pop()
+        # We keep a parallel mins stack which has a value for every entry in the
+        # value stack.
+        # This way, we can pop no matter what and still have the min. Think of
+        # it like snapshots: for every value element, we have a min element
+        # corresponding to the value element.
+        #
+        # Alternatively, we can keep a min stack which checks if the next value
+        # is the same value being popped.
+        p = self.values.pop()
+        m = self.mins.pop()
 
-        if p == self.minVal[-1]:
-            self.minVal.pop()
-            print(f"NEW MINVAL: {self.minVal}")
-
-        print(f"POPPPING {p}")
-
+        print(f"pop: v: {p} m: {m}")
         return p
 
     def top(self) -> int:
-        top = self.stack[-1]
-        print(f"TOP: {top}")
-        return top
+        return self.values[-1]
 
     def getMin(self) -> int:
-        print(f"RETURN MINVAL: {self.minVal}")
-        return self.minVal[-1]
-
-    def top(self) -> int:
-        top = self.stack[-1]
-        return top
-
-    def getMin(self) -> int:
-        return self.minVal[-1]
-
-    def getMin(self) -> int:
-        return self.minVal[-1]
-
-
-# This solution technically should not work since you're popping the minstack at
-# the same time as the value stack.
-#
-# class MinStack:
-#     def __init__(self):
-#         self.stack = []
-#         self.min_stack = []
-
-#     def push(self, val: int) -> None:
-#         self.stack.append(val)
-
-#         if self.min_stack:
-#             val = min(self.min_stack[-1], val)
-
-#         self.min_stack.append(val)
-
-#     def pop(self) -> None:
-#         self.stack.pop()
-#         self.min_stack.pop()
-
-#     def top(self) -> int:
-#         return self.stack[-1]
-
-#     def getMin(self) -> int:
-#         return self.min_stack[-1]
+        return self.mins[-1]
 
 
 def test_design_stack():
