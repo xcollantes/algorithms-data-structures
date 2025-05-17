c865421dc07faf87f8fc5e5b15280c94f3cc8acb
Xavier <collantes.xavier@gmail.com>
2025-05-16 20:19:23 -0700

    Design stack
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

---
+++ leetcode_questions/med/stacks_queues/design_stack.py
@@ -0,0 +1,47 @@
+"""
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+"""
+
+
+def test_design_stack():
+    minStack = MinStack()
+    minStack.push(-2)
+    minStack.push(0)
+    minStack.push(-3)
+    assert minStack.getMin() == -3
+    minStack.pop()
+    assert minStack.top() == 0
+    assert minStack.getMin() == -2
+
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if self.min_stack:
+            val = min(self.min_stack[-1], val)
+
+        self.min_stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
