2074b7e0bfa57edee4273814708da4947d25f03a
Xavier <collantes.xavier@gmail.com>
2025-05-16 20:18:00 -0700

    Move test
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- leetcode_questions/med/strings_arrays/spiral_matrix.py
+++ leetcode_questions/med/strings_arrays/spiral_matrix.py
@@ -13,6 +13,18 @@ n == matrix[i].length
 """
 
 
+def test_spiral_matrix():
+
+    # https://assets.leetcode.com/uploads/2020/11/13/spiral1.jpg
+    assert spiral_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) \
+        == [1, 2, 3, 6, 9, 8, 7, 4, 5]
+
+    # https://assets.leetcode.com/uploads/2020/11/13/spiral.jpg
+    assert spiral_matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]) \
+        == [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+
+    assert spiral_matrix([[2, 5], [8, 4], [0, -1]]) == [2, 5, 4, -1, 0, 8]
+
 def spiral_matrix(matrix: list[list[int]]) -> list[int]:
     ROWS = len(matrix)
     COLS = len(matrix[0])
