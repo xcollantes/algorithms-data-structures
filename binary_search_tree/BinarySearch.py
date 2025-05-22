87dabe5372339e1d40fa1808e7dbff1c961eafbe
Xavier <collantes.xavier@gmail.com>
2025-05-21 22:41:10 -0700

    Add working binary search
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- binary_search_tree/BinarySearch.py
+++ binary_search_tree/BinarySearch.py
@@ -7,10 +7,50 @@ def main():
     x = [3, 5, -1, 70, 45, -41, 0, 52, 6, 4, 78, 24, 34, 678, 89]
     x.sort()
 
-    print(BinarySearchIter(x, 1000))
+    # These are actually wrong but keeping since I wrote them when I was
+    # younger.
+    #
+    # assert BinarySearchIter(x, 1000) == False
+    # assert BinarySearchIter(x, 678) == True
 
-    print("Binary Search Recursive")
-    print(BinarySearchRec(x, 1000))
+    # Wrong because can't handle if target element is first element.
+    #
+    # assert BinarySearchIter(x, -41) == True
+
+    # print("Binary Search Recursive")
+    # assert BinarySearchRec(x, 1000) == False
+    # assert BinarySearchRec(x, 678) == True
+
+    print("Simple binary search")
+    assert simple_binary_search(x, 1000) == False
+    assert simple_binary_search(x, 678) == True
+    assert simple_binary_search(x, -41) == True
+
+
+def simple_binary_search(array: List[int], element: int) -> bool:
+    array.sort()  # Must be sorted for binary search to work.
+
+    left = 0
+
+    # Minus one to handle case where element does not exist on farthest right
+    # in array.
+    right = len(array) - 1
+
+    while left <= right:
+        # Until left pointer is equal to or less than right pointer to handle
+        # case where element is in farthest left in array.
+
+        mid = (left + right) // 2
+
+        if array[mid] == element:
+            return True
+
+        if array[mid] > element:
+            right = mid - 1
+        elif array[mid] < element:
+            left = mid + 1
+
+    return False
 
 
 def BinarySearchRec(list: List[int], element: int) -> bool:
