c69a2113e34ceafefcc45e8ebdc33e74b4126b88
Xavier <collantes.xavier@gmail.com>
2025-05-21 21:55:46 -0700

    Add min prefix
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

---
+++ leetcode_questions/easy/strings_arrays/min_prefix.py
@@ -0,0 +1,67 @@
+"""14. Longest Common Prefix
+
+Easy
+
+Write a function to find the longest common prefix string amongst an array of
+strings.
+
+If there is no common prefix, return an empty string "".
+
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters if it is non-empty.
+"""
+
+
+def test_min_prefix():
+    """pytest min_prefix.py"""
+    assert min_prefix(["flower", "flight", "flow"]) == "fl"
+    assert min_prefix(["dog", "racecar", "car"]) == ""
+    assert min_prefix(["flower", "flare", "flow", "flight", "flew"]) == "fl"
+
+
+def min_prefix(strs: list[str]) -> str:
+    """
+    flower == flare
+      i
+               x
+    fl == flow
+     i
+           x
+
+    fl == fot
+    i
+          x
+    """
+    compare = strs[0]
+    i = len(compare)
+
+    for word in strs[1:]:
+
+        print(f"{compare} {i} {word} compare {compare[:i]}")
+
+        # move pointer from right to left until matches.
+        # from there, match other words.
+        while compare[:i] != word[:i]:
+            i -= 1
+
+        # found match.
+        compare = compare[:i]
+
+    return compare
