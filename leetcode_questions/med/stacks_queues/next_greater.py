ad0e3b918c413a4a1a0a58aaa5f9c6e4c07426e1
Xavier <collantes.xavier@gmail.com>
2025-08-17 17:55:19 -0700

    Greater value
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

---
+++ leetcode_questions/med/stacks_queues/next_greater.py
@@ -0,0 +1,104 @@
+"""496. Next Greater Element I
+
+Easy
+
+The next greater element of some element x in an array is the first greater
+element that is to the right of x in the same array.
+
+You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1
+is a subset of nums2.
+
+For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j]
+and determine the next greater element of nums2[j] in nums2. If there is no next
+greater element, then the answer for this query is -1.
+
+Return an array ans of length nums1.length such that ans[i] is the next greater
+element as described above.
+
+
+Example 1:
+
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+- 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+- 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+
+
+Example 2:
+
+Input: nums1 = [2,4], nums2 = [1,2,3,4]
+Output: [3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+- 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+
+
+Constraints:
+
+1 <= nums1.length <= nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 104
+All integers in nums1 and nums2 are unique.
+All the integers of nums1 also appear in nums2.
+
+
+Follow up: Could you find an O(nums1.length + nums2.length) solution?
+"""
+
+
+def test_next_greater():
+    """pytest next_greater.py"""
+    assert next_greater([4, 1, 2], [1, 3, 4, 2]) == [-1, 3, -1]
+    assert next_greater([2, 4], [1, 2, 3, 4]) == [3, -1]
+
+
+def next_greater(nums1: list[int], nums2: list[int]) -> list[int]:
+    """
+    [4, 1, 2]
+
+    [1, 3, 4, 2]
+           i
+
+    stack =
+    map = {
+        1: 3
+        3: 4
+
+        # handle left in stack
+        4: -1
+
+        # handle rest in nums1
+        2: -1
+    }
+    res = -1 3 -1
+    """
+
+    hash = {}
+    stack = [nums2[0]]
+
+    # iterate on nums2 to create hashmap
+    for i in range(1, len(nums2)):
+
+        # if less than prev
+        if stack and stack[-1] < nums2[i]:
+            hash[stack[-1]] = nums2[i]
+
+            stack.pop()
+
+        stack.append(nums2[i])
+
+        print(f"i {i} stack {stack} hash {hash}")
+
+    # if anything left, add to hash
+    # these have not found a next greater
+    for e in stack:
+        hash[e] = -1
+
+    print(hash)
+
+    res = []
+    for n1 in nums1:
+        res.append(hash[n1])
+
+    return res
