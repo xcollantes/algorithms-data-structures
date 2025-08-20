105654a600214d9526f7d613af0ebf954f5984d6
Xavier <collantes.xavier@gmail.com>
2025-08-20 13:50:26 -0700

    Refactor total cost calculation logic for hiring workers using two min-heaps. Enhance test coverage and utilize infinity sentinel for empty heap selection.
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

---
+++ leetcode_questions/med/stacks_queues/asteroids.py
@@ -0,0 +1,73 @@
+"""735. Asteroid Collision
+
+Medium
+
+We are given an array asteroids of integers representing asteroids in a row. The
+indices of the asteroid in the array represent their relative position in space.
+
+For each asteroid, the absolute value represents its size, and the sign
+represents its direction (positive meaning right, negative meaning left). Each
+asteroid moves at the same speed.
+
+Find out the state of the asteroids after all collisions. If two asteroids meet,
+the smaller one will explode. If both are the same size, both will explode. Two
+asteroids moving in the same direction will never meet.
+
+
+Example 1:
+
+Input: asteroids = [5,10,-5]
+Output: [5,10]
+Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+
+
+Example 2:
+
+Input: asteroids = [8,-8]
+Output: []
+Explanation: The 8 and -8 collide exploding each other.
+
+
+Example 3:
+
+Input: asteroids = [10,2,-5]
+Output: [10]
+Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+
+
+Constraints:
+
+2 <= asteroids.length <= 104
+-1000 <= asteroids[i] <= 1000
+asteroids[i] != 0
+"""
+
+
+def test_asteroids():
+    """pytest asteroids.py"""
+    assert asteroids([5, 10, -5]) == [5, 10]
+    assert asteroids([8, -8]) == []
+    assert asteroids([10, 2, -5]) == [10]
+
+
+def asteroids(asteroids: list[int]) -> list[int]:
+
+    stack = []
+
+    for e in asteroids:
+        print(e)
+
+        while stack and e < 0:
+
+            if -e > stack[-1]:
+                stack.pop()
+                continue
+
+            elif -e == stack[-1]:
+                stack.pop()
+            break
+
+        else:
+            stack.append(e)
+
+    return stack
