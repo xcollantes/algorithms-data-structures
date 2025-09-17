09eea68069e80c27b2987374350c52888ff7a2a9
Xavier <collantes.xavier@gmail.com>
2025-09-17 16:58:36 -0700

    Update excel function to enhance performance and readability. Refactor logic for better clarity and streamline output for improved user feedback.
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

---
+++ leetcode_questions/easy/strings_arrays/repeat_substring.py
@@ -0,0 +1,42 @@
+"""459. Repeated Substring Pattern
+
+Easy
+
+Given a string s, check if it can be constructed by taking a substring of it and
+appending multiple copies of the substring together.
+
+
+Example 1:
+
+Input: s = "abab"
+Output: true
+Explanation: It is the substring "ab" twice.
+
+
+Example 2:
+
+Input: s = "aba"
+Output: false
+Example 3:
+
+Input: s = "abcabcabcabc"
+Output: true
+Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of lowercase English letters.
+"""
+
+
+def test_repeat_substring():
+    """pytest repeat_substring.py"""
+    assert repeat_substring("abab") == True
+    assert repeat_substring("aba") == False
+    assert repeat_substring("abcabcabcabc") == True
+
+
+def repeat_substring(s: str) -> bool:
+    """"""
