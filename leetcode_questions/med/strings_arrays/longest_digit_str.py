54203f9b15cabacc38e052f8908571a1a9cf4054
Xavier <collantes.xavier@gmail.com>
2025-09-20 21:12:23 -0700

    Add solution and notes.
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- leetcode_questions/med/strings_arrays/longest_digit_str.py
+++ leetcode_questions/med/strings_arrays/longest_digit_str.py
@@ -32,9 +32,36 @@ def test_longest_digit_str():
     """pytest longest_digit_str.py"""
     assert longest_digit_str(nums=[10, 2]) == "210"
     assert longest_digit_str(nums=[3, 30, 34, 5, 9]) == "9534330"
-    assert longest_digit_str(nums=[0, 9, 8, 7, 6, 5, 4, 3, 2, 1]) == "1098765432"
+    assert longest_digit_str(nums=[0, 9, 8, 7, 6, 5, 4, 3, 2, 1]) == "9876543210"
 
 
 def longest_digit_str(nums: list[int]) -> str:
     """
     """
+
+    arr = [str(n) for n in nums]
+
+    # print(arr)
+
+    # Python will sort alphabetically.
+    # So numbers as string will be [10, 2, 21, 22]
+
+    # at first i tried padding each string with "9". as many as the largest
+    # placed number. so if the longest number is 100: [100, 199, 299]
+
+    # but using 9 has limit with [3, 30, 34, 5, 9]
+    # [39, 30, 34, 59, 90] -> [30, 34, 39, 59, 90]
+    # where 34 (34) is before 3 (39)
+
+    arr.sort(reverse=True, key=lambda x: x * 10)
+
+    # for a in arr:
+    #     print(f"{a * 10}: {a}")
+
+    if arr[0] == "0":
+        return "0"
+
+    print(arr)
+
+    return "".join(arr)
+
